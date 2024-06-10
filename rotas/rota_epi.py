9411678875d9b0f590f7c98fe2ad4314ecfb34d9
Rapha-Cruz <raphajcruz@hotmail.com>
2024-06-10 07:54:44 -0300

    quinto

--- rotas/rota_epi.py
+++ rotas/rota_epi.py
@@ -10,7 +10,7 @@ def listar_epi():
     conn = criar_conexao()
     cursor = conn.cursor(dictionary=True)
 
-    cursor.execute("SELECT * FROM epi")
+    cursor.execute("SELECT * FROM epi ORDER BY DATA_VENCIMENTO DESC")
     epi = cursor.fetchall()
 
     #fechar conexão com banco de dados
